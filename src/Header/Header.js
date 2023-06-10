24eca688a58effdd04272ecbd61cc36f2491b3c5
Shruti <Shruti.2.goyal@bt.com>
2023-06-10 15:58:56 +0530

    Routes update

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     <div class="navbar">
       <a href="./" class="logo">Ghaziabad Toastmasters Club</a>
       <ul class="nav-links">
-        <li><a href={"/Home"}>Home</a></li>
+        <li><a href={process.env.PUBLIC_URL + "/Home"}>Home</a></li>
         <li><a href={"/Gallery"}>Gallery</a></li>
         <li><a href={"/Contact"}>Contact US</a></li>
       </ul>
